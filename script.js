0f91f88c9a09b13b02d8c52439caa0a36c55431f
Danil Razumkov <razumkov@e1.ru>
2017-11-21 19:28:07 +0500

    go

---
+++ script.js
@@ -0,0 +1,19 @@
+var status = "close";
+$('body').on('click', '.s_menu_icon', function(){
+  if(status == "close"){
+    status = "open";
+    $('.s_menu_content').animate({"width" : "100%"}, {duration:500, queue:false});
+    $('.s_menu_icon').animate({"border-radius" : "0"}, {duration:500, queue:false});
+    setTimeout(function () {
+      $('.s_menu_text').css({"display" : "inline"});
+    }, 600);
+  }
+  else if(status == "open"){
+    status = "close";
+    $('.s_menu_text').css({"display" : "none"});
+    setTimeout(function () {
+      $('.s_menu_content').animate({"width" : "0"}, {duration:500, queue:false});
+      $('.s_menu_icon').animate({"border-radius" : "10"}, {duration:500, queue:false});
+    }, 100);
+  }
+});
